3e75b33b8c28305e4493340b03f3d374a982017b
Chun <caochun@gmail.com>
2023-10-13 18:44:35 +0800

    init

--- com/anish/calabashbros/Sorter.java
+++
@@ -1,9 +0,0 @@
-package com.anish.calabashbros;
-
-public interface Sorter<T extends Comparable<T>> {
-    public void load(T[] elements);
-
-    public void sort();
-
-    public String getPlan();
-}
